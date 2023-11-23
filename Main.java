5d4b251d6855acc33c3e8a265dc0b3e868566483
victorsejourne <victor.sejourne@etu.amu-univ.fr>
2023-11-23 12:09:31 +0100

    + class UI

--- Main.java
+++ Main.java
@@ -1,11 +1,32 @@
+import UI.UI;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 // Classe Main pour l'exécution de la simulation
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("--- Welcome to Me at The zoo-scape ! ---");
+    public static void main(String[] args) throws IOException {
+//        // Setup terminal and screen layers
+//        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+//        Screen screen = new TerminalScreen(terminal);
+//        screen.startScreen();
+//
+//        // Create window to hold the panel
+//        BasicWindow window = new BasicWindow();
+//
+//        // Create gui and start gui
+//        MultiWindowTextGUI gui = new MultiWindowTextGUI(screen, new DefaultWindowManager(), new EmptySpace(TextColor.ANSI.BLACK));
+//        gui.addWindowAndWait(window);
+
+        UI ui = new UI();
+        ui.display('0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0');
+
     }
-
-
-
-
 
 }
---
+++ UI/UI.java
@@ -0,0 +1,47 @@
+package UI;
+
+public class UI {
+
+    int position;
+    public UI() {
+        position = 7;
+    }
+
+    public void display(char e0, char e1, char e2, char e3, char e4, char e5, char e6, char e7, char e8, char e9, char e10, char e11, char e12, char e13, char e14){
+        System.out.println("" +
+                "---------------------------------------------------\n" +
+                "|         |         |         |         |         |\n" +
+                "|         |         |         |         |         |                          o Key symbols :\n" +
+                "|    "+e0+"    |    "+e1+"    |    "+e2+"    |    "+e3+"    |    "+e4+"    |    => Enclosure\n" +
+                "|         |         |         |         |         |                          - X : Player\n" +
+                "|         |         |         |         |         |                          - ? : Empty enclosure\n" +
+                "---------------------------------------------------                          - ~ : Dirty enclosure\n" +
+                "(         (         (         (         (         (                          - ! : Hungry creatures\n" +
+                " )         )         )         )         )         )\n" +
+                "(    "+e5+"    (    "+e6+"    (    "+e7+"    (    "+e8+"    (    "+e9+"    (    => Aquarium\n" +
+                " )         )         )         )         )         )\n" +
+                "(         (         (         (         (         (\n" +
+                "---------------------------------------------------\n" +
+                "<         >         <         >         <         >\n" +
+                ">         <         >         <         >         <\n" +
+                "<    "+e10+"    >    "+e11+"    <    "+e12+"    >    "+e13+"    <    "+e14+"    >    => Aviary\n" +
+                ">         <         >         <         >         <\n" +
+                "<         >         <         >         <         >\n" +
+                "---------------------------------------------------\n" +
+                "\n" +
+                "o Selected enclosure :\n" +
+                "- enclosure type\n" +
+                "- creatures type\n" +
+                "- x/10\n" +
+                "- show creatures\n" +
+                "- enclosure dirtiness\n" +
+                "\n" +
+                "o Actions :\n" +
+                "- Move (U,D,L,R)\n" +
+                "- Clean (or C)\n" +
+                "- Feed (or F)\n" +
+                "\n" +
+                "- - - - - - - - - -\n" +
+                "*Licorn noises*");
+    }
+}
